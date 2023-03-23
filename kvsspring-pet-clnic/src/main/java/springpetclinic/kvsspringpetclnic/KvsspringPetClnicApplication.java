08818b38a0bd39b39c7094b2813989bfcb532231
kvsmanikanta <kvsmanikanta@gmail.com>
2023-03-24 05:19:28 +0530

    Initial Project Add

---
+++ kvsspring-pet-clnic/src/main/java/springpetclinic/kvsspringpetclnic/KvsspringPetClnicApplication.java
@@ -0,0 +1,13 @@
+package springpetclinic.kvsspringpetclnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvsspringPetClnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvsspringPetClnicApplication.class, args);
+	}
+
+}
---
+++ kvsspring-pet-clnic/src/test/java/springpetclinic/kvsspringpetclnic/KvsspringPetClnicApplicationTests.java
@@ -0,0 +1,13 @@
+package springpetclinic.kvsspringpetclnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvsspringPetClnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
